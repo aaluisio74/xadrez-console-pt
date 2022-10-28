a082ca7eb7a672a4869c63c4eb6a64437a46f102
aaluisio74 <aalusio@gmail.com>
2022-10-28 09:54:20 -0300

    Projeto criado

---
+++ xadrez-console-pt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace xadrez_console_pt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Olá Aluisio Anderson");
+        }
+    }
+}
